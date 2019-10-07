a1fc59f6b5207ce767ce9d42f9996e00ca925faf
Javier <javierfalca1@gmail.com>
2019-10-07 12:40:08 -0400

    Added Test Cases for Navigation Bar User Input - Added boilerplate code for the navigation bar for testing

---
+++ src/core/navbar/navbar.py
@@ -0,0 +1,17 @@
+class NavBar:
+    location = ""
+    money = 0
+    dates = []
+
+    def getLocation(self):
+        return self.location
+
+    def getMoneyAmount(self):
+        return self.money
+
+    def getDates(self):
+        return self.dates
+
+
+
+
