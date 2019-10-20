03ffa55a7d247ec4f919f14d450c86757b79effb
Javier <javierfalca1@gmail.com>
2019-10-20 17:38:16 -0400

    Added Javascript for interfacing front end to backend

--- src/core/server/server.py
+++ src/core/server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request
 from src.core.flights.flights import HermesFlights
 from src.core.localstorage import LocalStorage
 from selenium import webdriver
@@ -12,17 +12,15 @@ app = Flask(__name__,template_folder=root_dir(), static_folder=root_dir())
 
 @app.route('/')
 def index():
-  print(root_dir()[:-15])
+  # print(root_dir()[:-15])
+
   return render_template('index.html')
 
 @app.route('/get-flights/')
 def my_link():
   fl = HermesFlights()
+
   return fl.getFlightData("BUF-sky", "2019-10-23")
 
 if __name__ == '__main__':
   app.run(debug=True)
-
-
-def getLocalStorageData():
-  storage = LocalStorage(webdriver.Firefox())
