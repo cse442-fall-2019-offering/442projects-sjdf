c164419ac99c97cae5f4383c7ac10b6d3cc9da47
Javier <javierfalca1@gmail.com>
2019-10-20 20:28:27 -0400

    Implemented flight frontend to read JSON
    JSON is hosted on /get-flights/
    
    All available flights are displayed on screen

--- src/js/filterFlights.js
+++ src/js/filterFlights.js
@@ -1,6 +1,6 @@
 
 function ajaxGetRequest(path){
-    let request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if (this.readyState === 4 && this.status === 200){
             console.log(this.response);
@@ -11,13 +11,13 @@ function ajaxGetRequest(path){
 }
 
 function filterFlights(path){
-    let location = document.getElementById("location").value;
-    let price = document.getElementById("price").value;
-    let currencyType = document.getElementById("currencyType").value;
-    let startdate = document.getElementById("startdate").value;
-    let enddate = document.getElementById("enddate").value;
+    var location = document.getElementById("current_location").value;
+    var price = document.getElementById("price").value;
+    var currencyType = document.getElementById("currencyType").value;
+    var startdate = document.getElementById("startdate").value;
+    var enddate = document.getElementById("enddate").value;
     userInput = [location, price, currencyType, startdate, enddate];
-    convertUserInput(userInput);
+    //convertUserInput(userInput);
 
 
     //Security needed here
